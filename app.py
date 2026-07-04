2270c2f043042666056635ddf91bfac483aadd59
vidyasagargg <vidyasagargummerlair20@gmail.com>
2026-07-04 08:31:34 +0530

    Initialize Flask with configuration
    Initialize Flask with configuration

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 #Need to update code logic for backend application here
 
+from flask import Flask, jsonify, request
+import sqlite3
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+DB_FILE = "vet_clinic.db"
